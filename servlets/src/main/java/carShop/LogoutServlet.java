33197c7e9e7b2eebbe8ce722d0e94d0a80eb0a54
Zemledukh <zemledukhd@gmail.com>
2013-12-16 13:28:19 +0200

    X

--- servlets/src/main/java/carShop/LogoutServlet.java
+++ servlets/src/main/java/carShop/LogoutServlet.java
@@ -15,7 +15,7 @@ public class LogoutServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
         HttpSession session = req.getSession(false);
         if ( session != null) session.invalidate();  	      
-        resp.sendRedirect("guestWelcomePage.jsp");     
+        resp.sendRedirect("guestWelcomePage.jsp");      
 	}
 	
 }
